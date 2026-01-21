1b5667da4c9031d9e22f902d0123569687e97a46
Oviya Shree S <oviyashree.s2023@vitstudent.ac.in>
2026-01-21 12:45:49 +0530

    Added attendance marking and viewing logic

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print("Student Attendance Project Started")
+students = {}
+
+def mark_attendance(name, status):
+    students[name] = status
+
+def view_attendance():
+    print("Attendance List:")
+    for name, status in students.items():
+        print(name, "-", status)
+
+mark_attendance("Oviya", "Present")
+mark_attendance("Pradeep", "Absent")
+view_attendance()
